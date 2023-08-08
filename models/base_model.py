4aca4b98c588e9a918774bfc292f56027dfb602b
Favour04 <idaeworfavour1@gmail.com>
2023-08-08 21:37:50 +0100

    kwargs updated

--- models/base_model.py
+++ models/base_model.py
@@ -11,14 +11,14 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.fromisoformat(str(datetime.now()))
-        self.updated_at = datetime.fromisoformat(str(datetime.now()))
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at':
-                    key = datetime.fromisoformat(value)
+                    kwargs[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key == 'updated_at':
-                    key = datetime.fromisoformat(value)
+                    kwargs[key] = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
             self.__dict__.update(kwargs)
             
@@ -29,16 +29,15 @@ class BaseModel:
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.updated_at = datetime.fromisoformat(str(datetime.now()))
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        dict = self.__dict__
+        dict = {}
+        dict.update(self.__dict__)
         dict["__class__"] = f"{self.__class__.__name__}"
         if not isinstance(dict["created_at"], str):
-            dict["created_at"] = \
-                    f"{dict['created_at'].strftime('%Y-%m-%dT%H:%M:%S.%f')}"
+            dict["created_at"] = self.updated_at.isoformat()
         if not isinstance(dict["updated_at"], str):
-            dict["updated_at"] = \
-                    f"{dict['updated_at'].strftime('%Y-%m-%dT%H:%M:%S.%f')}"
+            dict["updated_at"] = self.updated_at.isoformat()
         return dict
