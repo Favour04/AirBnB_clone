a63f2cb406af9f6aeea5909f60454c3770fdc38e
favour04 <idaeworfavour1@gmail.com>
2024-03-24 20:03:52 +0100

    document base model and city

--- models/base_model.py
+++ models/base_model.py
@@ -1,3 +1,10 @@
+"""
+This module contains the BaseModel class.
+
+BaseModel is the base class for all other classes in the project.
+It defines common attributes and methods that are inherited by other classes.
+"""
+
 #!/usr/bin/python3
 from datetime import datetime
 from models import storage
@@ -5,6 +12,18 @@ from uuid import uuid4
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the BaseModel class.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Attributes:
+            id (str): The unique identifier of the instance.
+            created_at (datetime): The datetime when the instance was created.
+            updated_at (datetime): The datetime when the instance was last updated.
+        """
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
@@ -18,13 +37,28 @@ class BaseModel:
             storage.new(self)
         
     def __str__(self):
+        """
+        Returns a string representation of the instance.
+
+        Returns:
+            str: The string representation of the instance.
+        """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     
     def save(self):
+        """
+        Updates the updated_at attribute with the current datetime and saves the instance.
+        """
         self.updated_at = datetime.now()
         storage.save()
     
     def to_dict(self):
+        """
+        Converts the instance to a dictionary representation.
+
+        Returns:
+            dict: The dictionary representation of the instance.
+        """
         dict = {}
         dict.update(self.__dict__)
         dict["__class__"] = f"{self.__class__.__name__}"
--- models/city.py
+++ models/city.py
@@ -1,10 +1,28 @@
+"""
+    This module defines the City class, which represents a city in the AirBnB clone project.
+    It contains attributes such as state_id and name.
+"""
 #!/usr/bin/python3
 from models.base_model import BaseModel
 from datetime import datetime
 from models import storage
 
 class City(BaseModel):
+    """
+    City class that inherits from BaseModel.
+    Attributes:
+        state_id (str): The ID of the state to which the city belongs.
+        name (str): The name of the city.
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new City instance.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         super().__init__()
         self.state_id = ""
         self.name = ""
